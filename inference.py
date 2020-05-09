87f53b8db7926ed43e2f48069745bcd0077b5cd5
angakoko <angakoko@gmail.com>
2020-05-09 12:27:25 +0100

    Load model

--- inference.py
+++ inference.py
@@ -35,7 +35,7 @@ class Network:
     """
 
     def __init__(self):
-        ### TODO: Initialize any class variables desired ###
+        ###: Initialize any class variables desired ###
         self.plugin = None
         self.network = None
         self.input_blob = None
@@ -43,13 +43,41 @@ class Network:
         self.exec_network = None
         self.infer_request = None
 
-    def load_model(self):
-        ### TODO: Load the model ###
-        ### TODO: Check for supported layers ###
-        ### TODO: Add any necessary extensions ###
-        ### TODO: Return the loaded inference plugin ###
-        ### Note: You may need to update the function parameters. ###
-        return
+    def load_model(self, model, device = "CPU", cpu_extension = None):
+        ### Load the model ###
+        model_xml = model
+        model_bin = os.path.splitext(model_xml)[0] + ".bin"
+
+        # Initialize the plugin
+        self.plugin = IECore()
+
+        # Add a CPU extension, if applicable
+        if cpu_extension and "CPU" in device:
+            self.plugin.add_extension(cpu_extension, device)
+
+        # Read the IR as a IENetwork
+        self.network = IENetwork(model=model_xml, weights=model_bin)
+
+        ###: Check for supported layers ###
+        if "CPU" in device:
+            supported_layers = self.plugin.query_network(self.network, "CPU")
+            not_supported_layers = [l for l in self.network.layers.keys() if l not in supported_layers]
+            if len(not_supported_layers) != 0:
+                log.error("Following layers are not supported by the plugin for specified device {}:\n {}".
+                        format(device, ', '.join(not_supported_layers)))
+                log.error("Please try to specify cpu extensions library path in sample's command line parameters using -l "
+                        "or --cpu_extension command line argument")
+                sys.exit(1)
+
+        # Load the IENetwork into the plugin
+        self.exec_network = self.plugin.load_network(self.network, device)
+
+        # Get the input layer
+        self.input_blob = next(iter(self.network.inputs))
+        self.output_blob = next(iter(self.network.outputs))
+
+        ###: Return the loaded inference plugin ###
+        return self.plugin
 
     def get_input_shape(self):
         ### TODO: Return the shape of the input layer ###
